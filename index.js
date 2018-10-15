416c447a414435215c770c21eeed030077c79dcd
Max Wolf <maxcharleswolf@gmail.com>
2018-10-15 17:24:49 -0500

    adding Google OAuth. Added heroku

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ rang: "dipkin" });
-});
+//generic register for passport to register strategies
+passport.use(new GoogleStrategy()); //creates new instance of google passport strategy. pass in config
 
 //if there is a port assigned by heroku, use PORT, otherwise, use 5000
 const PORT = process.env.PORT || 5000; //heroku's oportunity to pass in the correct port to use
