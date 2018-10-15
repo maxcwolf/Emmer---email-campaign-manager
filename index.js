394b418db8f90366bff7d124138d7a7792fa7b44
Max Wolf <maxcharleswolf@gmail.com>
2018-10-15 16:56:25 -0500

    test commit for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ rang: "dipkin" });
 });
 
 //if there is a port assigned by heroku, use PORT, otherwise, use 5000
